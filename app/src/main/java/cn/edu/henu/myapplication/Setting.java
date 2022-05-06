cf0313c9c2c5c25dca014832eb9f79ee6311cdae
zhangkaibin0921 <3301043642@qq.com>
2022-05-06 23:01:17 +0800

    新建activity_setting

--- app/src/main/java/cn/edu/henu/myapplication/Setting.java
+++ app/src/main/java/cn/edu/henu/myapplication/Setting.java
@@ -1,6 +1,12 @@
 package cn.edu.henu.myapplication;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Setting extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
 }
