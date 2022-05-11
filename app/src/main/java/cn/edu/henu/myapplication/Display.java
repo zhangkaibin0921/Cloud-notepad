e649d644b65f506c355bb79695060ca4d190d530
zhangkaibin0921 <3301043642@qq.com>
2022-05-11 14:27:31 +0800

    recycleview大致结构实现

---
+++ app/src/main/java/cn/edu/henu/myapplication/Display.java
@@ -0,0 +1,14 @@
+package cn.edu.henu.myapplication;
+
+public class Display {
+    private String content;
+    private int id;
+
+    public Display(String content) {	this.content = content;	}
+
+    public void setID(int id){	this.id = id ;	}
+
+    public String getContent() {	return content;	}
+
+    public int getId(){	return id;	}
+}
