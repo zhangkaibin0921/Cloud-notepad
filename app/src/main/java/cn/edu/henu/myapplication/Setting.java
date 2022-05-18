cbd3995f678041ab34805887bae7f6907262aa93
DHB-2003 <80146082+DHB-2003@users.noreply.github.com>
2022-05-18 20:39:46 +0800

    setting

--- app/src/main/java/cn/edu/henu/myapplication/Setting.java
+++ app/src/main/java/cn/edu/henu/myapplication/Setting.java
@@ -1,6 +1,7 @@
 package cn.edu.henu.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,5 +9,8 @@ public class Setting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+    }
+
+    public void back(View view) {
     }
 }
