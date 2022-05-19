a8b16e7db3486ab11d95d2ab2e5fe426786bd206
breeze-666 <2458156548@qq.com>
2022-05-19 20:33:28 +0800

    bmob相关

--- app/src/main/java/cn/edu/henu/myapplication/LogIn/BmobManager.java
+++ app/src/main/java/cn/edu/henu/myapplication/LogIn/BmobManager.java
@@ -1,2 +1,37 @@
-package cn.edu.henu.myapplication.LogIn;public class BmobManager {
+package cn.edu.henu.myapplication.LogIn;
+
+import android.content.Context;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobUser;
+
+public class BmobManager {
+    private static final String BMOB_SDK_ID = "4c33e339432a88901c207e9a7a7114ee";//自己的bmob账号
+    private volatile static BmobManager mInstance = null;
+    private BmobManager() {
+
+    }
+    public static BmobManager getInstance() {
+        if (mInstance == null) {
+            synchronized (BmobManager.class) {
+                if (mInstance == null) {
+                    mInstance = new BmobManager();
+                }
+            }
+        }
+        return mInstance;
+    }
+    //初始化Bmob
+    public void initBmob(Context mContext) {
+
+        Bmob.initialize(mContext, BMOB_SDK_ID);
+    }
+    // 手机登录成功后获取本地对象
+    public MyUser getUser() {
+        return BmobUser.getCurrentUser(MyUser.class);
+    }
+    // 是否登录
+    public boolean isLogin() {
+        return BmobUser.isLogin();
+    }
 }
