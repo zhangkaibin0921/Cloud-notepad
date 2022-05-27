21637e8847d2bfb02acd8408deee0824cec806df
breeze-666 <2458156548@qq.com>
2022-05-27 09:38:04 +0800

    个人信息可以添加到云端数据库

--- app/src/main/java/cn/edu/henu/myapplication/PersonInfo.java
+++ app/src/main/java/cn/edu/henu/myapplication/PersonInfo.java
@@ -1,20 +1,64 @@
 package cn.edu.henu.myapplication;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import cn.bmob.v3.Bmob;
 import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
 import cn.edu.henu.myapplication.LogIn.UserInfoDB;
+import cn.edu.henu.myapplication.widget.TitleLayout;
 
 public class PersonInfo extends AppCompatActivity {
-    private TextView username;
+    EditText ID,username,sex,live_place,birthday;
+    TitleLayout titleLayout;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bmob.initialize(this, "4c33e339432a88901c207e9a7a7114ee");
         setContentView(R.layout.activity_person_info);
+        ID = findViewById(R.id.content_ig_id);
+        username = findViewById(R.id.content_ig_name);
+        sex = findViewById(R.id.content_ig_gender);
+        live_place = findViewById(R.id.content_ig_region);
+        birthday = findViewById(R.id.content_ig_brithday);
+        titleLayout = findViewById(R.id.tl_title);
+        titleLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateUser(view);
+
+            }
+        });
+    }
+    private void updateUser(final View view){
+        final UserInfoDB user = BmobUser.getCurrentUser(UserInfoDB.class);
+        user.setUsername(username.getText().toString());
+        user.setSex(sex.getText().toString());
+        user.setLive_place(live_place.getText().toString());
+        user.setBirthday(birthday.getText().toString());
+        user.update(new UpdateListener() {
+            @Override
+            public void done(BmobException e) {
+                if (e == null) {
+                    Snackbar.make(view, "更新用户信息成功：" , Snackbar.LENGTH_LONG).show();
+                } else {
+                    Snackbar.make(view, "更新用户信息失败：" + e.getMessage(), Snackbar.LENGTH_LONG).show();
+                    Log.e("error", e.getMessage());
+                }
+
+            }
+        });
 
     }
+
 }
